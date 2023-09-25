f1aa654308f6199bdb946726b01a14a94128390a
Nicolas Fuentes <nfuentes11@gmail.com>
2023-09-25 06:17:55 -0400

    Basic library logic

---
+++ script.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(title, author, read) {
+  this.titel = title;
+  this.author = author;
+  this.read = false;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
