0cb28bb8e4d0233098473948cff98df6bf922a74
Nicolas Fuentes <nfuentes11@gmail.com>
2023-09-26 06:16:20 -0400

    basic layout and style

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function addBookToLibrary(book) {
 const beHereNow = new Book("Be Here now", "Dam rass", false);
 const dailyStoic = new Book("Daily Stoic", "Ryan Holiday", false);
 const youCantHurtMe = new Book("You can't hurt me", "David Goggins", true);
+const fiveRings = new Book("The Book of Five Rings", "Miyamoto Musashi", true);
 const body = document.body;
 const div = document.createElement("div");
 div.classList.add("container");
@@ -22,6 +23,18 @@ body.append(div);
 myLibrary.forEach((book) => {
   const card = document.createElement("div");
   card.classList.add("card");
-  card.textContent = `${book.title}, by ${book.author}, Read: ${book.read}`;
+  card.textContent = `${book.title}\nby ${book.author}\n\nRead: ${book.read}`;
   div.appendChild(card);
+});
+
+//Add new book button and modal
+const addBookButton = document.querySelector(".new-book");
+const closeButton = document.querySelector(".close-bnt");
+const modal = document.querySelector(".modal");
+
+addBookButton.addEventListener("click", () => {
+  modal.showModal();
+});
+closeButton.addEventListener("click", () => {
+  modal.close();
 });
