b28f30e1f86369ddb48323d827070779bbf6652a
Nicolas Fuentes <nfuentes11@gmail.com>
2023-09-25 21:12:26 -0400

    displaying library array inside of cards in the browser

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ function addBookToLibrary(book) {
 
 const beHereNow = new Book("Be Here now", "Dam rass", false);
 const dailyStoic = new Book("Daily Stoic", "Ryan Holiday", false);
+const youCantHurtMe = new Book("You can't hurt me", "David Goggins", true);
+const body = document.body;
+const div = document.createElement("div");
+div.classList.add("container");
+body.append(div);
 
-const body = document.querySelector("body");
-body.append(myLibrary);
+myLibrary.forEach((book) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.textContent = `${book.title}, by ${book.author}, Read: ${book.read}`;
+  div.appendChild(card);
+});
